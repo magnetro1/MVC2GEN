952e5005487a66e5db3f710fa1bc27b7b1b47aa0
magnetro1 <magnetro2k@gmail.com>
2022-06-26 20:17:36 -0700

    Reverted to using strings instead of arrays

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { Knockdown_State, StagesTable, ProxBlock, namesTable} from './constObjects/staticData.js';
 import * as data from './csv_to_js/Shuma47_node.js';
-import * as fs from 'fs'
+import * as fs from 'fs';
 
 // console.log(Knockdown_State);
 // console.log(StagesTable);
@@ -8,18 +8,17 @@ import * as fs from 'fs'
 // console.log(namesTable);
 
 //Inchoate Map method:
-var x  = data.P2_A_Animation_Timer_Main
-var y  = data.P2_A_Knockdown_State
-// var z  = data.P2_A_Airborne
+var x  = data.P2_A_Animation_Timer_Main.split(',');
+var y  = data.P2_A_Knockdown_State.split(',');
+// var z  = data.P2_A_Airborne;
 
 var result = x.map(function(e,i){
   var a = x[i]
   var b = y[i]
-
   return a > 0 && b == 32
 })
 
-fs.writeFile("P2_A_BeingHit.js", `export var P2_A_BeingHit = [${result.toString()}];`, (err) => {
+fs.writeFile("P2_A_BeingHit.js", `export var P2_A_BeingHit = "${result.toString()}";`, (err) => {
     if (err)
       console.log(err);
     else {
