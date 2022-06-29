e5fe3823bbef9613071bfb5fc00586961b7f8f95
magnetro1 <magnetro2k@gmail.com>
2022-06-29 01:24:37 -0700

    updated the loop structure and added comments & a table list

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
-import { Knockdown_State, StagesTable, ProxBlock, namesTable} from './main_files/staticData.js';
+import { Knockdown_State, StagesTable, ProxBlock, namesTable} from './main_files/staticData.js'
 // import * as data from './main_files/Shuma47_node.js';
 import * as data from './main_files/CaptainCommandoRogueCable8_node.js';
 import * as fs from 'fs';
-const slots = ["P1_A_","P1_B_","P1_C_","P2_A_","P2_B_","P2_C_"];
 
+const slots = ["P1_A_","P1_B_","P1_C_","P2_A_","P2_B_","P2_C_"];
+const clipLength = data.A_2D_Game_Timer.split(',')
+
+/*List of States I'm going to export data for:
+  I was thinking of adding a table of states and their corresponding logics...
+  It would be cool if the MVC2GEN could iterate through this table automatically 
+  to pick out which key and value pairs to use for each character.
+
+	"Being_Hit" 			: Animation_Timer_Main > 0 && Knockdown_State == 32 
+	"Flying_Screen_Air"		: FlyingScreen == 1 && Knockdown_State == 32 && Airborne == 2
+	"FlyingScreen_OTG"		:
+	"FSD_Dash"				:
+	"FS_Install_1"			:
+	"FS_Install_2"			:
+	"NJ_Air"				:
+	"NJ_Rising" 			:
+	"OTG_Extra_Stun" 		:
+	"OTG_Forced_Stun" 		:
+	"OTG_Hit" 				:
+	"OTG_Roll_Invincible" 	:
+	"OTG_Roll_Stunned" 		:
+	"ProxBlock_Air" 		:
+	"ProxBlock_Ground" 		:
+	"Pushblock_Air" 		:
+	"Pushblock_Ground" 		:
+	"Rising_Invincibility" 	:
+	"SJ_Air" 				:
+	"SJ_Counter" 			:
+	"Stun" 					:
+	"Thrown_Air" 			:
+	"Thrown_Ground" 		:
+*/
+
+/*Instead for now, there's these arrays corresponding to each character.
+I have to create a set of Arrays for each character for now.
+*/
 var BeingHitArray = [];
 	BeingHitArray[0] = '';
 	BeingHitArray[1] = '';
@@ -20,7 +55,25 @@ var FlyingScreenAirArray = [];
 	FlyingScreenAirArray[4] = '';
 	FlyingScreenAirArray[5] = '';
 
-for (var  m = 0 ; m < BeingHitArray.length ; m++ )
+for ( let sixCharsI in slots )
+{
+	let tempTxt = '';
+	for ( let frame in clipLength )
+	{
+		tempTxt += ( eval(`data.${slots[sixCharsI]}FlyingScreen.split(',')`)[frame] == 1 
+			&& eval(`data.${slots[sixCharsI]}Knockdown_State.split(',')`)[frame] == 32
+			&& eval(`data.${slots[sixCharsI]}Airborne.split(',')`)[frame] == 2) 
+			? 1 + ',' : 0 + ',';
+		FlyingScreenAirArray[sixCharsI] = `export var ${slots[sixCharsI]}FlyingScreenAir = "${tempTxt}";`;
+	}
+	tempTxt = '';
+	FlyingScreenAirArray[sixCharsI] = FlyingScreenAirArray[sixCharsI].replace(`,"` , `"`);
+}
+
+/*Old method of doing the loops; I'm keeping it here to update it later.
+I'd like to figure out an efficient way to do these loops before propagating it to the other files.
+
+for (let  m = 0 ; m < slots.length ; m++ )
 {
 	for ( let i = 0; i < data.A_2D_Game_Timer.split(',').length ; i++ )
 	{
@@ -33,29 +86,23 @@ for (var  m = 0 ; m < BeingHitArray.length ; m++ )
 	// console.log(slots[m]+"BeingHit: " + BeingHitArray[m]);
 }
 console.log(BeingHitArray);
+*/
 
-for (var  m = 0 ; m < FlyingScreenAirArray.length ; m++ )
-{
-	for ( let i = 0; i < data.A_2D_Game_Timer.split(',').length ; i++ )
-	{
-		// console.log(slots[m]+"FlyingScreen: "+(eval(`data.${slots[m]}FlyingScreen.split(',')`)[i]))
-		// console.log(slots[m]+"Knockdown_State: "+(eval(`data.${slots[m]}Knockdown_State.split(',')`)[i]))
-		// console.log(slots[m]+"Airborne: "+(eval(`data.${slots[m]}Airborne.split(',')`)[i]))
-		FlyingScreenAirArray[m] += ( eval(`data.${slots[m]}FlyingScreen.split(',')`)[i] == 1 
-			&& eval(`data.${slots[m]}Knockdown_State.split(',')`)[i] == 32
-			&& eval(`data.${slots[m]}Airborne.split(',')`)[i] == 2) 
-			? 1 + ',' : 0 + ','
-	}
-	// console.log(slots[m]+"FlyingScreenAir: " + FlyingScreenAirArray[m]);
-}
-console.log(FlyingScreenAirArray);
+console.log(FlyingScreenAirArray[0]);
+// console.log(clipLength.length);
+// console.log(FlyingScreenAirArray[0].length);
+// console.log(FlyingScreenAirArray[1].length);
+// console.log(FlyingScreenAirArray[2].length);
+// console.log(FlyingScreenAirArray[3].length);
+// console.log(FlyingScreenAirArray[4].length);
+// console.log(FlyingScreenAirArray[5].length);
 
 // fs.writeFile(`P2_C_BeingHit.js`, `export var P2_C_BeingHit = "${P2_C_BeingHit.toString()}";`, (err) =>
 // {
 // 	 if (err)
 // 	   console.log(err);
 // 	 else {
-// 	   console.log('Hola');
+// 	   console.log('Test');
 // 	 }
 // });
 
