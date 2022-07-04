6d4532c1d7f9969958b88d248bfd99421115121c
magnetro1 <magnetro2k@gmail.com>
2022-07-04 09:42:37 -0700

    re-organized and formatted most of the script

--- testJS.js
+++
@@ -1,7 +0,0 @@
-import * as fs from 'fs';
-import * as path from 'path';
-const dirPath = path.join(process.cwd(), `/exportToAE/testPath}`);
-
-fs.mkdirSync(dirPath);
-
-fs.writeFile(`${dirPath}/'Test.js`, 'Helpcontent', (err) => { });
