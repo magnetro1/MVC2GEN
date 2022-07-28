ea553bc87012fad125534692219172549b24e720
magnetro1 <magnetro2k@gmail.com>
2022-07-28 07:27:22 -0700

    Cleaned CSVToJs & added Magneto105 data

--- resources/PCSX2RR_and_CheatEngine/CSV_Input/BK/CSVToJS_V2.ps1
+++ resources/PCSX2RR_and_CheatEngine/CSV_Input/BK/CSVToJS_V2.ps1
@@ -1,29 +1,30 @@
-﻿# Get the path to the input file from the command line.
+﻿# Get the CSV from the BAT file 
 $CleanArgs = $args.Split(',').Trim()
 $FileObject = (Get-ChildItem $CleanArgs)
 
-# Get data from the supplied csv file.
+# Get data from the supplied CSV file.
 $CSVFile = Import-CSV $CleanArgs
 $FileName = $FileObject.Name.ToString().replace(".csv", ".jsx")
 
-# First line of CSVFile is our Headers but Powershell does some lifting for me here.
+# First line of CSVFile is our Headers
 $Headers = ($CSVFile | Get-Member -MemberType 'NoteProperty' | Select-Object -ExpandProperty 'Name')
 
-# Powershell one-liner to gather and arrange data for us.
-$Data = $Headers | ForEach-Object {Write-Output "$(($CSVFile | Select-Object -ExpandProperty $_) -Join ',')"}
+# Arrange data
+$Data = $Headers | ForEach-Object {
+    Write-Output "$(($CSVFile | Select-Object -ExpandProperty $_) -Join ',')"
+}
 
-# Initialize some vars.
+
 $OutputFile = "$($FileObject.DirectoryName)\$($FileName)"
 $OutputString = "{`n"
-$shortString = ''
 
 # Format our strings.
 For ($i = 0; $i -lt $Headers.Count; $i++) {
     # If we find the special headers then we turn them into quote-wrapped strings.
     If ($Headers[$i] -eq "P1_Input_T3" -or $Headers[$i] -eq "P2_Input_T3") {
-        # $Data[$i] = ($Data[$i].Split(",") | ForEach-Object { $(If ($_ -ne "") {"`"$($_)`""} Else {""}) }) -Join ","
         $OutputString += "`"$($Headers[$i])`":`"$($Data[$i])`",`n"
-    } Else {
+    }
+    Else {
         $OutputString += "`"$($Headers[$i])`":[$($Data[$i])],`n"
     }
 }
@@ -33,4 +34,4 @@ $OutputString = $OutputString -Replace ".$", ""
 $OutputString += "}"
 
 # Output the thing.
-$OutputString | Out-File -FilePath $OutputFile -Encoding "ASCII"
+$OutputString | Out-File -FilePath '..\main_files\'$OutputFile -Encoding "ASCII"
