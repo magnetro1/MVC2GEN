2a9914684d7aee4ef02cff62c0e4013b2c278330
magnetro1 <60054111+magnetro1@users.noreply.github.com>
2022-12-11 18:37:16 -0800

    single quote replacement test2

--- evalExp.js
+++ evalExp.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const {KNOCKDOWN_STATE_OBJ, PROX_BLOCK_OBJ, NAME_TABLE_OBJ, FLOATING_POINT_ADRS, MIN_MAX_ADRS, MISC_ADRS, STAGES_OBJ, PORTRAITS_TO_TIME_OBJ} = require("./main_files/staticData");
+const {KNOCKDOWN_STATE_OBJ, PROX_BLOCK_OBJ, NAME_TABLE_OBJ, FLOATING_POINT_ADRS, MIN_MAX_ADRS, MISC_ADRS, STAGES_OBJ, PORTRAITS_TO_TIME_OBJ} = require('./main_files/staticData');
 
 const DO_ROM_FILES = false; // Skip ROM logic files
 
@@ -20,13 +20,13 @@ let tempMinMaxBuffer = '';
 import(ORG_JS_FILE)
   .then((orgData) => // Imports Object with key : value pairs
   {
-    const CLIP_LENGTH = orgData.A_2D_Game_Timer.split(",").length;
+    const CLIP_LENGTH = orgData.A_2D_Game_Timer.split(',').length;
     for (adr in MIN_MAX_ADRS)
     {
       const KEY = MIN_MAX_ADRS[adr];
       const VALUE = orgData[MIN_MAX_ADRS[adr]];
-      const MIN = Math.min(...VALUE.split(","));
-      const MAX = Math.max(...VALUE.split(","));
+      const MIN = Math.min(...VALUE.split(','));
+      const MAX = Math.max(...VALUE.split(','));
       let tempMin = [];
       let tempMax = [];
 
@@ -35,8 +35,8 @@ import(ORG_JS_FILE)
         tempMax[clipLen] = MAX;
         tempMin[clipLen] = MIN;
       }
-      tempMinMaxBuffer += `exports.${ KEY }_Max = "${ tempMax }";\n`;
-      tempMinMaxBuffer += `exports.${ KEY }_Min = "${ tempMin }";\n`;
+      tempMinMaxBuffer += `exports.${ KEY }_Max = '${ tempMax }';\n`;
+      tempMinMaxBuffer += `exports.${ KEY }_Min = '${ tempMin }';\n`;
     }
     console.log(tempMinMaxBuffer);
   });
